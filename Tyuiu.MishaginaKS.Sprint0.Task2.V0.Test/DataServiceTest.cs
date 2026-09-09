72ba803ec6e918a944d9d679a35294eb709d132f
MishaginaKS <kseniamishagina@mail.ru>
2026-09-09 19:51:10 +0500

    Task2

---
+++ Tyuiu.MishaginaKS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MishaginaKS.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MishaginaKS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChectGetMessegeValid()
+        {
+            var name = "Ксюша";
+            var res = DataService.GetMessege(name);
+
+            Assert.AreEqual("Привет..., Ксюша", res);
+
+        }
+    }
+}
---
+++ Tyuiu.MishaginaKS.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MishaginaKS.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MishaginaKS.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7afecf02-efd5-4aa3-9048-a6b292b0499a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
